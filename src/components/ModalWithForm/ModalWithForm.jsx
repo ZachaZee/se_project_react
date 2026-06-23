28017f1b506c2fdbae087135f11936f860223a9a
Zach Zabriskie <90725150+ZachaZee@users.noreply.github.com>
2026-06-23 21:54:15 +0000

    part 9

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,47 +1,18 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm() {
+function ModalWithForm({ children, title, buttonText}) {
   return (
     <div className="modal">
         <div className="modal__content">
-            <h2 className="modal__title">New Garment</h2>
+            <h2 className="modal__title">{title}</h2>
         <button type="button" className="modal__close">
           CLOSE
         </button>
       <form className="modal__form">
-        <label htmlFor="name" className="modal__label">
-          Name
-          <input
-            type="text"
-            className="modal__input"
-            id="name"
-            placeholder="name"
-          />
-        </label>
-        <label htmlFor="imageUrl" className="modal__label">
-          Image{" "}
-          <input
-            type="url"
-            className="modal__input"
-            id="imageUrl"
-            placeholder="Image URL"
-          />
-        </label>
-        <fieldset className="radiobuttons">
-          <legend className="modal__legend">"Select the weather type:"</legend>
-          <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input id="hot" type="radio" className="modal__radio-input" />Hot
-          </label>
-
-          <label htmlFor="warm" className="modal__label modal__label_type_radio">
-            <input id="warm" type="radio" className="modal__radio-input" />Warm
-          </label>
-
-          <label htmlFor="cold" className="modal__label modal__label_type_radio">
-            <input id="cold" type="radio" className="modal__radio-input" />Cold
-          </label>
-        </fieldset>
-        <button type="sumbit" className="modal__submit"></button>
+        {children}
+        <button type="sumbit" className="modal__submit">
+          {buttonText}
+        </button>
       </form>
       </div>
     </div>
