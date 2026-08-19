a662dc722e37a697036521881c749edaa2e013f5
Zach Zabriskie <zachzee365@gmail.com>
2026-08-18 19:56:46 -0700

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__name">Zachary Zabriskie</div>
+      <div className="year">2026</div>
+    </footer>
+  );
+}
+
+export default Footer;
